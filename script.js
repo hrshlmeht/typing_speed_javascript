ff1179a4169c0aea08dece1f156279b1ed476c77
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-26 13:22:35 -0400

    typing speed game completed

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const RANDOM_QUOTE_API = 'http://api.quotable.io/random'
 const  quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input' , () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    let correct = true 
+    arrayQuote.forEach((characterSpan, index )=>{
+        const character = arrayValue[index]
+        if (character == null){
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+
+    } )
+    if (correct) renderNewQuote()
+})
 
 function getRandomQuote(){
    return fetch(RANDOM_QUOTE_API)
@@ -13,11 +37,26 @@ async function renderNewQuote(){
     quoteDisplayElement.innerText = ''
     quote.split('').forEach(character =>{
         const characterSpan = document.createElement('span')
-        characterSpan.classList.add('correct')
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     })
     quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime 
+function startTimer(){
+    timerElement.innerText = 0
+    startTime = new Date() 
+    setInterval(()=>{
+        timer.innerText = getTimerTime()
+    },1000
+    )
+
+    
+}
+function getTimerTime(){
+    return (new Date() - startTime)/1000
 }
 
 renderNewQuote()
