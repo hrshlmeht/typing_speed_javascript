bebf510b5512baf4df18964592ee815649f68ef4
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-26 14:07:24 -0400

    1 minute functionality added

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
-const RANDOM_QUOTE_API = 'http://api.quotable.io/random'
-const  quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-const timerElement = document.getElementById('timer')
+const RANDOM_QUOTE_API = 'http://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+const resultsElement = document.getElementById('results');
 
-quoteInputElement.addEventListener('input' , () => {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-    const arrayValue = quoteInputElement.value.split('')
-    let correct = true 
-    arrayQuote.forEach((characterSpan, index )=>{
-        const character = arrayValue[index]
-        if (character == null){
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.remove('incorrect')
-            correct = false
-        } else if (character === characterSpan.innerText){
-            characterSpan.classList.add('correct')
-            characterSpan.classList.remove('incorrect')
+let totalCharactersTyped = 0;
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
         } else {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.add('incorrect')
-            correct = false
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            correct = false;
         }
+    });
 
-    } )
-    if (correct) renderNewQuote()
-})
+    if (correct && arrayValue.length === arrayQuote.length) {
+        totalCharactersTyped += quoteInputElement.value.length;
+        renderNewQuote(); // Render new quote after the current quote is finished
+    }
+});
 
-function getRandomQuote(){
-   return fetch(RANDOM_QUOTE_API)
-    .then(response => response.json())
-    .then(data => data.content )
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API)
+        .then(response => response.json())
+        .then(data => data.content);
 }
 
-async function renderNewQuote(){
-    const quote = await getRandomQuote()
-    quoteDisplayElement.innerText = ''
-    quote.split('').forEach(character =>{
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText = character
-        quoteDisplayElement.appendChild(characterSpan)
-    })
-    quoteInputElement.value = null
-    startTimer()
+async function renderNewQuote() {
+    quoteInputElement.value = ''; // Clear the input after the quote is finished
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerText = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    });
 }
 
-let startTime 
-function startTimer(){
-    timerElement.innerText = 0
-    startTime = new Date() 
-    setInterval(()=>{
-        timer.innerText = getTimerTime()
-    },1000
-    )
-
-    
+function startTimer() {
+    totalCharactersTyped = 0;
+    timerElement.innerText = 0;
+    startTime = new Date();
+    const timerInterval = setInterval(() => {
+        const elapsedTime = getTimerTime();
+        timerElement.innerText = elapsedTime;
+        if (elapsedTime >= 60) {
+            clearInterval(timerInterval);
+            resultsElement.innerText = `Total characters typed: ${totalCharactersTyped}`;
+            renderNewQuote(); // Render new quote after the timer is completed
+        }
+    }, 1000);
 }
-function getTimerTime(){
-    return (new Date() - startTime)/1000
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000);
 }
 
-renderNewQuote()
+renderNewQuote();
+startTimer();
