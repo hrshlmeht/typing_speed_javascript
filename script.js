0671831819c02ddb27fd936678eb3616662037e1
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-22 18:29:58 -0400

    Added the new function of just getting the words

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const RANDOM_QUOTE_API = 'https://api.quotable.io/random';
-const quoteDisplayElement = document.getElementById('quoteDisplay');
+const RANDOM_WORD_API = 'https://random-words-api.vercel.app/word';
+const quoteDisplayElement = document.getElementById('quoteDisplay'); 
 const quoteInputElement = document.getElementById('quoteInput');
 const timerElement = document.getElementById('timer');
 const resultsElement = document.getElementById('results');
 var startButton = document.getElementById('startButton');
+var startbutton2 = document.getElementById('startButton2');
 const submitButton = document.getElementById("button-submitted");
 const startContainer = document.querySelector(".start-container");
 var container = document.querySelector('.container');
@@ -28,34 +30,6 @@ preventCopyPasteContainer.addEventListener("paste", function(event) {
   event.preventDefault();
 })
 
-//*********function to calculate the words typed per minute.*************/
-// quoteInputElement.addEventListener('input', () => {
-//     const arrayQuote = quoteDisplayElement.querySelectorAll('span');
-//     const typedValue = quoteInputElement.value.trim();
-//     const typedWords = typedValue.split(' ');
-//     let correct = true;
-//     arrayQuote.forEach((characterSpan, index) => {
-//         const word = typedWords[index];
-//         if (word == null) {
-//             characterSpan.classList.remove('correct');
-//             characterSpan.classList.remove('incorrect');
-//             correct = false;
-//         } else if (word === characterSpan.innerText) {
-//             characterSpan.classList.add('correct');
-//             characterSpan.classList.remove('incorrect');
-//         } else {
-//             characterSpan.classList.remove('correct');
-//             characterSpan.classList.add('incorrect');
-//             correct = false;
-//         }
-//     });
-
-//     if (correct && typedWords.length === arrayQuote.length) {
-//         correctWords++;
-//         renderNewQuote(); // Render new quote after the current quote is finished
-//     }
-// });
-
 submitButton.addEventListener("click", function(event) {
     // Prevent form submission (if using within a form)
     event.preventDefault();
@@ -70,7 +44,10 @@ submitButton.addEventListener("click", function(event) {
     formContainer.style.display = 'none'
     
   });
-quoteInputElement.addEventListener('input', () => {
+
+let quoteMode = true;
+
+const quoteModeInputEventListener = () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span');
     const arrayValue = quoteInputElement.value.split('');
     let correct = true;
@@ -95,16 +72,47 @@ quoteInputElement.addEventListener('input', () => {
         totalCharactersTyped += valueWithoutSpaces.length;
         renderNewQuote(); // Render new quote after the current quote is finished
     }
-});
+};
 
+const wordModeInputEventListener = () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            correct = false;
+        }
+    });
 
+    if (correct && arrayValue.length === arrayQuote.length) {
+        const valueWithoutSpaces = quoteInputElement.value.replace(/\s/g, '');
+        totalCharactersTyped += valueWithoutSpaces.length;
+        renderNewWord(); // Render new word after the current word is finished
+    }
+};
 
-
+quoteInputElement.addEventListener('input', quoteModeInputEventListener);
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API)
         .then(response => response.json())
         .then(data => data.content);
+}
+
+function getRandomWord() {
+    return fetch(RANDOM_WORD_API)
+        .then(response => response.json())
+        .then(data => data[0].word); // Access the "word" property from the first element of the array
 }
 
 async function renderNewQuote() {
@@ -118,26 +126,16 @@ async function renderNewQuote() {
     });
 }
 
-// timer function to calcualte words per minute
-// function startTimer() {
-//     totalCharactersTyped = 0;
-//     timerElement.innerText = 0;
-//     startTime = new Date();
-//     let timerInterval;
-
-//     timerInterval = setInterval(() => {
-//         const elapsedTime = getTimerTime();
-//         timerElement.innerText = elapsedTime;
-//         if (elapsedTime >= 60) {
-//             clearInterval(timerInterval);
-//             const wordsPerMinute = Math.round(correctWords / (elapsedTime / 60));
-//             resultsElement.innerText = `Times up! \u{1F641}\nYou typed: ${wordsPerMinute} words per minute! \u{1F600}\u{1F44D}`;
-//             renderNewQuote(); // Render new quote after the timer is completed
-//         }
-//     }, 1000);
-
-//     setTimeout(showResultTab, 60000); // Show the result tab after 60 seconds
-// }
+async function renderNewWord() {
+    quoteInputElement.value = ''; // Clear the input after the quote is finished
+    const quote = await getRandomWord();
+    quoteDisplayElement.innerText = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    });
+}
 
 function startTimer() {
     totalCharactersTyped = 0;
@@ -158,6 +156,24 @@ function startTimer() {
     setTimeout(showResultTab, 60000); // Show the result tab after 60 seconds
 }
 
+function startTimer2() {
+    totalCharactersTyped = 0;
+    timerElement.innerText = 0;
+    startTime = new Date();
+    let timerInterval;
+    
+    timerInterval = setInterval(() => {
+        const elapsedTime = getTimerTime();
+        timerElement.innerText = elapsedTime;
+        if (elapsedTime >= 60) {
+            clearInterval(timerInterval);
+            resultsElement.innerText = `Times up! \u{1F641}\nYou typed: ${totalCharactersTyped} chars in a minute! \u{1F600}\u{1F44D}`;
+            renderNewWord(); // Render new quote after the timer is completed
+        }
+    }, 1000);
+
+    setTimeout(showResultTab, 60000); // Show the result tab after 60 seconds
+}
 
 function showResultTab() {
     const resultTab = document.getElementById("results");
@@ -169,11 +185,28 @@ function getTimerTime() {
 }
 
 startButton.addEventListener('click', function() {
+    // Start in quote mode
+    quoteMode = true;
+    quoteInputElement.removeEventListener('input', wordModeInputEventListener);
+    quoteInputElement.addEventListener('input', quoteModeInputEventListener);
     startButton.style.display = 'none';
     formContainer.style.display = 'none'
     container.style.display = 'block';
     greetingContainer.style.display = 'none';
     renderNewQuote();
     startTimer();
-  
-  });
+});
+
+startbutton2.addEventListener('click', function() {
+    // Start in word mode
+    quoteMode = false;
+    quoteInputElement.removeEventListener('input', quoteModeInputEventListener);
+    quoteInputElement.addEventListener('input', wordModeInputEventListener);
+    startButton.style.display = 'none';
+    startbutton2.style.display = 'none'
+    formContainer.style.display = 'none'
+    container.style.display = 'block';
+    greetingContainer.style.display = 'none';
+    renderNewWord();
+    startTimer2();
+});
